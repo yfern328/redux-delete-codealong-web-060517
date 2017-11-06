2f4fa949d51eb2a280b086c95043be926f8437d8
yfern328 <yfernan2@u.rochester.edu>
2017-11-05 19:23:34 -0500

    added delete functionality to todo list

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CreateTodo store={this.props.store} />
-        <Todos store={this.props.store} />
+        <CreateTodo store={ this.props.store } />
+        <Todos store={ this.props.store } />
       </div>
     );
   }
